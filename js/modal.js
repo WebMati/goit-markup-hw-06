cd15168675baba3f9a1a84e91194496d65522098
Mateusz Bajsarowicz <94319299+WebMati@users.noreply.github.com>
2023-01-31 09:22:35 +0100

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -8,9 +8,13 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("backdrop")) {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  });
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-
